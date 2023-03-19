6677a553eabbc42d236d7ea76e4be13d1197c9a5
Ali <121503489+AlirezaAnzali@users.noreply.github.com>
2023-03-19 13:07:44 -0400

    fix endpoints

--- routes/videos.js
+++ routes/videos.js
@@ -1,17 +1,24 @@
-import videos from "../data/videos.json"
-import videoDetails from "../data/video-details.json"
+const videos = require("../data/videos.json");
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/videos", (req, res) => {
-  res.json(videos);
+router.get("/", (req, res) => {
+  const filteredVideos = videos.map((video) => {
+    return {
+      id: video.id,
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+    };
+  });
+  res.json(filteredVideos);
 });
 
-router.get("/videos/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
-  if (videos.includes(videos.id)) {
-    res.send("That ingridient is in stock");
+  const video = videos.find((video) => video.id === id);
+  if (video) {
+    res.send(video);
   } else {
     res.send("No video with that id exists");
   }
